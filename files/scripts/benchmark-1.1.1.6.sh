ba856e03e6bd4f6078d936aed8a4d02e40287841
Proletaryo Cabales <proletaryo@gmail.com>
2017-03-15 14:05:06 +0800

    added custom facts / checkers for cis benchmarch 1.1.1.1-8

---
+++ files/scripts/benchmark-1.1.1.6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MODULE=squashfs
+RESULT=$(/sbin/lsmod | /bin/grep $MODULE)
+
+if [[ -z $RESULT ]]
+then
+  # module is not loaded in kernel, check if this was remediated
+  RESULT=$(/bin/grep "install[[:space:]+]$MODULE[[:space:]+]/bin/true" /etc/modprobe.d/*)
+  if [[ $RESULT ]]
+  then
+    echo 'cis_benchmark_1_1_1_6=pass'
+  else
+    echo 'cis_benchmark_1_1_1_6=fail'
+  fi
+else
+  echo 'cis_benchmark_1_1_1_6=fail'
+fi
