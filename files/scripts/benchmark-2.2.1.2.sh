88acfc141f3425429a2ecb9781da17f815ba232e
Proletaryo Cabales <proletaryo@gmail.com>
2017-03-24 13:17:38 +0800

    added check scripts for 2.2.1.3 to 3.2.5

--- files/scripts/benchmark-2.2.1.2.sh
+++ files/scripts/benchmark-2.2.1.2.sh
@@ -4,9 +4,40 @@
 
 BENCHMARKNUM='cis_benchmark_2_2_1_2'
 
-RESULT=
+RESULT=0
 
-if [[ -z $RESULT ]]
+if [[ $( /bin/rpm -qa ntp ) ]]
+then
+  NTPCONFIGFILE=/etc/ntp.conf
+
+  if [[ -e $NTPCONFIGFILE ]]
+  then
+    LINES_RESTRICT=$( /bin/grep '^restrict' $NTPCONFIGFILE )
+
+    if [[ ! -z $LINES_RESTRICT ]]
+    then
+      LINES_SERVER=$( /bin/grep '^server' $NTPCONFIGFILE )
+
+      if [[ ! -z $LINES_SERVER ]]
+      then
+        LINES_OPTIONS=$( /bin/grep '^OPTIONS="-u ntp:ntp"' /etc/sysconfig/ntpd )
+
+        if [[ ! -z $LINES_OPTIONS ]]
+        then
+          RESULT=1
+        fi
+      else
+        RESULT=1
+      fi
+    else
+      RESULT=1
+    fi
+  else
+    RESULT=1
+  fi
+fi
+
+if [[ $RESULT -eq 0 ]]
 then
   echo "${BENCHMARKNUM}=passed"
 else
