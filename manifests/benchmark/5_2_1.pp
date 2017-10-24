87073c5ed62c4573d5fdf12966b91f1c5407600f
patrickgenio <patrick.genio@voyagerinnovation.com>
2017-10-24 14:40:51 +0800

    added manifests/benchmark/5_2_1.pp

---
+++ manifests/benchmark/5_2_1.pp
@@ -0,0 +1,17 @@
+# 5.2.1    Ensure permissions on /etc/ssh/sshd_config are configured (Scored)
+class ciscentos6::benchmark::5_2_1 {
+
+  if ! defined(Ciscentos6::Common::Set_file_permissions['/etc/ssh/sshd_config']) {
+    ciscentos6::common::set_file_permissions { '/etc/ssh/sshd_config':
+      benchmark_number => '5.2.1',
+      benchmark_status => $cis_benchmark_5_2_1,
+      permissions    => 'og-rwx',
+    }
+  }
+  else {
+    ciscentos6::common::just_relay_fact_value { '5.2.1':
+      benchmark_number => '5.2.1',
+      benchmark_status => $cis_benchmark_5_2_1,
+    }
+  }
+}
