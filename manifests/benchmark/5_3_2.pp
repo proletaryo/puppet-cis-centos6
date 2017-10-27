b9eb95a742570c8cb20a35bf33115fb136d8f9c5
patrickgenio <patrick.genio@voyagerinnovation.com>
2017-10-27 11:41:42 +0800

    added benchamrks 5_3_2.pp & 5_3_3.pp

--- manifests/benchmark/5_3_2.pp
+++ manifests/benchmark/5_3_2.pp
@@ -1,45 +1,46 @@
-# 5.3.1    Ensure password creation requirements are configured (Scored)
-class ciscentos6::benchmark::5_3_1 {
+# 5.3.2    Ensure lockout for failed password attempts is configured (Scored)
+class ciscentos6::benchmark::5_3_2 {
   if ! defined(Exec['/etc/pam.d/password-auth']) {
-    if defined('$cis_benchmark_5_3_1') {
-      if $cis_benchmark_5_3_1 == 'failed' {   # remediate
-        $addline = 'password requisite pam_cracklib.so try_first_pass retry=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1'
-        $regex = '^password.+pam_cracklib.so'
+    if defined('$cis_benchmark_5_3_2') {
+      if $cis_benchmark_5_3_2 == 'failed' {   # remediate
+        $addline = 'auth required pam_faillock.so preauth audit silent deny=5 unlock_time=900\nauth [success=1 default=bad] pam_unix.so\nauth [default=die] pam_faillock.so authfail audit deny=5 unlock_time=900\nauth sufficient pam_faillock.so authsucc audit deny=5 unlock_time=900'
+        $regex = '^auth.+pam_unix.so'
         exec {'/etc/pam.d/password-auth':
           command => "echo $addline >> /etc/pam.d/password-auth",
           path    => "/bin",
           unless  => "grep -P $regex /etc/pam.d/password-auth",
+          require => Exec['etc/pam.d/password-auth'],
         }
         exec {'/etc/pam.d/system-auth':
           command => "echo $addline >> /etc/pam.d/system-auth",
           path    => "/bin",
           unless  => "grep -P $regex /etc/pam.d/system-auth",
+          require => Exec['etc/pam.d/system-auth'],
         }
         exec {'etc/pam.d/password-auth':
           command => "sed -i '/$regex/c\\$addline' /etc/pam.d/password-auth",
           path    => "/bin",
           onlyif  => "grep -P $regex /etc/pam.d/password-auth",
-          require => Exec['/etc/pam.d/password-auth'],
         }
         exec {'etc/pam.d/system-auth':
           command => "sed -i '/$regex/c\\$addline' /etc/pam.d/system-auth",
           path    => "/bin",
           onlyif  => "grep -P $regex /etc/pam.d/system-auth",
-          require => Exec['/etc/pam.d/system-auth'],
         }
       }
       else {
-        notice( "CIS Benchmark 5.3.1 : $cis_benchmark_5_3_1")
+        notice( "CIS Benchmark 5.3.2 : $cis_benchmark_5_3_2")
       }
     }
+    # grep -P '^auth.+required' /etc/pam.d/password-auth
     else {
       fail('Error: Externals facts were not defined. Make sure facter version is 1.7+ and externals facts are in /etc/facter/facts.d/)')
     }
   }
   else {
-    ciscentos6::common::just_relay_fact_value { '5.3.1':
-      benchmark_number => '5.3.1',
-      benchmark_status => $cis_benchmark_5_3_1,
+    ciscentos6::common::just_relay_fact_value { '5.3.2':
+      benchmark_number => '5.3.2',
+      benchmark_status => $cis_benchmark_5_3_2,
     }
   }
 }
--- manifests/benchmark/5_3_3.pp
+++ manifests/benchmark/5_3_3.pp
@@ -1,35 +1,34 @@
-# 5.3.1    Ensure password creation requirements are configured (Scored)
-class ciscentos6::benchmark::5_3_1 {
+# 5.3.3    Ensure password reuse is limited (Scored)
   if ! defined(Exec['/etc/pam.d/password-auth']) {
-    if defined('$cis_benchmark_5_3_1') {
-      if $cis_benchmark_5_3_1 == 'failed' {   # remediate
-        $addline = 'password requisite pam_cracklib.so try_first_pass retry=3 minlen=14 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1'
-        $regex = '^password.+pam_cracklib.so'
+    if defined('$cis_benchmark_5_3_2') {
+      if $cis_benchmark_5_3_2 == 'failed' {   # remediate
+        $addline = 'password sufficient pam_unix.so remember=5'
+        $regex = '^password.+sufficient.+pam_unix.so'
         exec {'/etc/pam.d/password-auth':
           command => "echo $addline >> /etc/pam.d/password-auth",
           path    => "/bin",
           unless  => "grep -P $regex /etc/pam.d/password-auth",
+          require => Exec['etc/pam.d/password-auth'],
         }
         exec {'/etc/pam.d/system-auth':
           command => "echo $addline >> /etc/pam.d/system-auth",
           path    => "/bin",
           unless  => "grep -P $regex /etc/pam.d/system-auth",
+          require => Exec['etc/pam.d/system-auth'],
         }
         exec {'etc/pam.d/password-auth':
-          command => "sed -i '/$regex/c\\$addline' /etc/pam.d/password-auth",
+          command => "sed -i 's/^password.\+sufficient.\+pam_unix.so/password sufficient pam_unix.so remember=5/' /etc/pam.d/password-auth",
           path    => "/bin",
           onlyif  => "grep -P $regex /etc/pam.d/password-auth",
-          require => Exec['/etc/pam.d/password-auth'],
         }
         exec {'etc/pam.d/system-auth':
-          command => "sed -i '/$regex/c\\$addline' /etc/pam.d/system-auth",
+          command => "sed -i 's/^password.\+sufficient.\+pam_unix.so/password sufficient pam_unix.so remember=5/' /etc/pam.d/system-auth",
           path    => "/bin",
           onlyif  => "grep -P $regex /etc/pam.d/system-auth",
-          require => Exec['/etc/pam.d/system-auth'],
         }
       }
       else {
-        notice( "CIS Benchmark 5.3.1 : $cis_benchmark_5_3_1")
+        notice( "CIS Benchmark 5.3.2 : $cis_benchmark_5_3_2")
       }
     }
     else {
@@ -37,9 +36,9 @@ class ciscentos6::benchmark::5_3_1 {
     }
   }
   else {
-    ciscentos6::common::just_relay_fact_value { '5.3.1':
-      benchmark_number => '5.3.1',
-      benchmark_status => $cis_benchmark_5_3_1,
+    ciscentos6::common::just_relay_fact_value { '5.3.2':
+      benchmark_number => '5.3.2',
+      benchmark_status => $cis_benchmark_5_3_2,
     }
   }
 }
