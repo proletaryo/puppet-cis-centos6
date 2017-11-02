405788cd11484839fc694d167e14f03e16fc8ecc
patrickgenio <patrick.genio@voyagerinnovation.com>
2017-11-02 16:17:30 +0800

    new benchmarks 3_6_2.pp to 3_6_4.pp

---
+++ manifests/benchmark/3_6_2.pp
@@ -0,0 +1,15 @@
+# 3.6.2    Ensure default deny firewall policy (Scored)
+class ciscentos6::benchmark::3_6_2 {
+  if $benchmark_status == 'failed' {   # remediate
+    exec {'implement a default DROP policy':
+      command => "iptables -P INPUT DROP; iptables -P OUTPUT DROP; iptables -P FORWARD DROP",
+      path    => "/bin:/sbin",
+    } ->
+    notify{ "CIS Benchmark 3.6.2 : remediated":
+      loglevel => notice,
+    }
+  }
+  else {
+    notice( "CIS Benchmark 3.6.2 : $cis_benchmark_3_6_2")
+  }
+}
---
+++ manifests/benchmark/3_6_3.pp
@@ -0,0 +1,15 @@
+# 3.6.3    Ensure loopback traffic is configured (Scored)
+class ciscentos6::benchmark::3_6_3 {
+  if $benchmark_status == 'failed' {   # remediate
+    exec {'implement the loopback rules':
+      command => "iptables -A INPUT -i lo -j ACCEPT; iptables -A OUTPUT -o lo -j ACCEPT; iptables -A INPUT -s 127.0.0.0/8 -j DROP",
+      path    => "/bin:/sbin",
+    } ->
+    notify{ "CIS Benchmark 3.6.3 : remediated":
+      loglevel => notice,
+    }
+  }
+  else {
+    notice( "CIS Benchmark 3.6.3 : $cis_benchmark_3_6_3")
+  }
+}
