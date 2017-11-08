f3ddba1b50bf23d34ed19ad6357c0f45616b5307
patrickgenio <patrick.genio@voyagerinnovation.com>
2017-11-08 18:22:21 +0800

    new benchmark 6_2_5.pp

---
+++ manifests/benchmark/6_2_5.pp
@@ -0,0 +1,16 @@
+# 6.2.5    Ensure root is the only UID 0 account (Scored)
+class ciscentos6::benchmark::6_2_5 {
+  if $cis_benchmark_6_2_5 == 'failed' {   # remediate
+    exec {'6_2_5 remediate':
+      command => "userlist=\$(cat /etc/passwd | awk -F: '(\$3 == 0 && \$1 != \"root\") { print \$1 }' | awk '{print \$0}'); for user in \$userlist; do uid=\$(cat /etc/passwd | awk -F: '{ print \$3 }' | sort -n | tail -1 | awk '{print \$0+1}'); sed -i \"s/^\\(\$user:[^:]\\):[0-9]*:/\\1:\$uid:/\" /etc/passwd; done",
+      path    => "/bin:/sbin:/usr/bin",
+      provider => shell,
+    } ->
+    notify{ "CIS Benchmark 6.2.5 : remediated":
+      loglevel => notice,
+    }
+  }
+  else {
+    notice( "CIS Benchmark 6.2.5 : $cis_benchmark_6_2_5")
+  }
+}
