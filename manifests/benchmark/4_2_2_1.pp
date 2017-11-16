dbb80461c0877a376119bc58377b2f568d7ec7da
patrickgenio <patrick.genio@voyagerinnovation.com>
2017-11-16 10:37:22 +0800

    enabled syslog-ng

--- manifests/benchmark/4_2_2_1.pp
+++ manifests/benchmark/4_2_2_1.pp
@@ -2,7 +2,7 @@
 class ciscentos6::benchmark::4_2_2_1 {
   if $cis_benchmark_4_2_2_1 == 'failed' {   # remediate
     exec {'enable syslog-ng':
-      command => "chkconfig syslog-ng off",
+      command => "chkconfig syslog-ng on",
       path    => "/bin:/sbin",
     } ->
     notify{ "CIS Benchmark 4.2.2.1 : remediated":
